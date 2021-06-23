38262e634408d96cb6736689230397e5555bca12
Ivan <vanuha277@gmail.com>
2021-06-23 10:19:30 +0300

    refactoring by cycle 'for'

--- src/index.js
+++ src/index.js
@@ -21,23 +21,21 @@ class Board extends React.Component {
   }
 
   render() {
+    const diws = [];
+    let num = 0;
+
+    for (var i = 0; i < 3; i++) {
+      let squares = [];
+      for (var j = 0; j < 3; j++) {
+        squares[j] = this.renderSquare(num);
+        num++;
+      }
+      diws.push(<div className="board-row" key = { i }>{ squares }</div>);
+    }
+    
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        { diws }
       </div>
     );
   }
